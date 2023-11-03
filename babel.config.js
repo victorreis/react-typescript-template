f6577bb5dacbaae8508504a37f6c497e5b6a3d9d
victorreis <v.crisostomo.reis@gmail.com>
2023-11-03 14:18:04 -0300

    chore: add missing plugin to babel config file

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
     ],
   ],
   plugins: [
+    '@babel/plugin-proposal-private-property-in-object',
     '@babel/plugin-transform-modules-commonjs',
     [
       '@babel/plugin-transform-react-jsx',
